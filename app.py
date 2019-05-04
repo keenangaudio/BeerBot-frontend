37d283757790dcfebeb08d4215ecf26b4ef7f1d3
Sunny Nagam <sunnynagam1@gmail.com>
2019-05-04 16:10:32 -0600

    sending images

--- app.py
+++ app.py
@@ -1,12 +1,32 @@
 import requests
+import os
 
+url = 'http://10.0.0.234:5000'
 
-data_example = {'temp_1': 100, 'temp_2': 150}
+json_data_example = {'This': 69, 'Is a test': 420}
 
-def sendData(data):
-	response = requests.post('http://10.0.0.234:5000/pi', json=data)
+def sendJsonData(json):
+	response = requests.post(url+'/pi', json=json)
 	if response.ok:
-	    print(response.json()) #--> {'temp_1': 100, 'temp_2': 150}
+		print(response.json())
+
+def sendImageData(img_url):
+	image = open(img_url, 'rb')
+	name_img = os.path.basename(img_url)
+	files= {'image': (name_img,image,'multipart/form-data',{'Expires': '0'}) }
+
+	response = requests.post(url+'/detect', files=files)
+
+	if response.ok:
+		print(response)
+
+	image.close()
 
 if __name__ == '__main__':
-    sendData(data_example)
+	# JSON example
+	#sendJsonData(json_data_example)
+
+	# Image example
+	
+	sendImageData('cat.jpg')
+	
